8c713c848743e6fd0d44048e9ecf0771b694301f
MisenEko <mysen79@gmail.com>
2022-08-24 23:05:32 +0200

    todo-list finish
    it was fun

--- todo-list-nodejs/app.js
+++ todo-list-nodejs/app.js
@@ -1,24 +1,60 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const port = 3000;
 
-const tasks = [
-    {
-        title: "Apprendre à programmer",
-        done: false,
-    },
-    {
-        title: "Faire les courses",
-        done: true,
-    }
-];
+// const tasks = [
+//     {
+//         title: "Apprendre à programmer",
+//         done: false,
+//     },
+//     {
+//         title: "Faire les courses",
+//         done: true,
+//     }
+// ];
 
-console.log(tasks)
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
 
 app.set('view engine', 'ejs');
 
+app.post('/task', (req, res)=>{
+    if(req.body.task){
+
+        req.session.tasks.push({
+            title: req.body.task,
+            done: false,
+        })
+    }
+        res.redirect('/')
+})
+
+app.get('/task/:id/done', (req, res)=>{
+    if(req.session.tasks[req.params.id]){        
+        req.session.tasks[req.params.id].done = true;
+    }
+    res.redirect('/')
+})
+
+app.get('/task/:id/delete', (req,res)=>{
+    if(req.session.tasks[req.params.id]){ 
+        req.session.tasks.splice(req.params.id, 1);
+    }
+    res.redirect('/')
+})
+
 app.get('/', (req, res)=>{
-    res.render('todolist', {tasks});
+    if(!req.session.tasks){
+        req.session.tasks = [];
+    }
+    res.render('todolist', {tasks: req.session.tasks});
 });
 
 app.listen(port, ()=>{
